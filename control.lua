14ea7628b747dd97033ea2ab7dc5f5615d265175
Jan Mischak <jan.mischak@kstp.at>
2021-01-16 04:32:39 +0100

    Fix gui not getting created in existing games
    Add some player removed handling

--- control.lua
+++ control.lua
@@ -3,11 +3,23 @@ local gui_handler = require("__JanSharpsGuiLibrary__.gui-handler")
 
 require("position-gui")
 
+local function register_player(player)
+  local inst = gui_handler.create(player.gui.screen, "position-gui", nil, player)
+  global.position_guis[player.index] = inst
+end
+
+local function deregister_player(player_index)
+  -- global.position_guis[player_index]:destroy() -- doesn't work. was bugged the entire time in the library. nice :D
+  global.position_guis[player_index] = nil
+end
+
 script.on_event(defines.events.on_player_created, function(event)
   local player = game.get_player(event.player_index)
+  register_player(player)
+end)
 
-  local inst = gui_handler.create(player.gui.screen, "position-gui", nil, player)
-  global.position_guis[event.player_index] = inst
+script.on_event(defines.events.on_player_removed, function(event)
+  deregister_player(event.player_index)
 end)
 
 script.on_event("TASDev-add-positions", function(event)
@@ -16,4 +28,8 @@ end)
 
 script.on_init(function()
   global.position_guis = {}
+
+  for _, player in pairs(game.players) do
+    register_player(player)
+  end
 end)
